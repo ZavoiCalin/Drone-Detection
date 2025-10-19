0319f6c5b34bdeebfae3d6e3809cfdf3b43695cd
ZavoiCalin <62338985+ZavoiCalin@users.noreply.github.com>
2025-10-19 13:05:54 +0300

    Update drone follower 1302

---
+++ drone_detection_follower1302/launch/detection_follower.launch.py
@@ -0,0 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='drone_detection_follower',
+            executable='drone_detection_follower',
+            name='drone_detection_follower',
+            output='screen'
+        ),
+    ])
